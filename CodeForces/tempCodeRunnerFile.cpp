fdd6cad11d567b0665291b6a89b53c1cebbed15c
Dhanush Hebbar <121495892+ErascusPlatypus@users.noreply.github.com>
2025-02-25 17:07:36 +0530

    Delete CodeForces directory

--- CodeForces/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-    int l, r, k;
-        cout << "Enter l, r, k: ";
-        cin >> l >> r >> k;
-
-        int result = countOperations(l, r, k);
-        cout << "Maximum possible operations: " << result << endl;
